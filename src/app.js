e37f8cc18c8e9ac503469570148bd8f709ed3340
Peyman Mortazavi <pey.mortazavi@gmail.com>
2016-07-24 17:24:51 -0700

    added test handler

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+var express = require('express');
+var app = express();
+
+app.get('/test', (request, response) => {
+    response.send('Heya');
+})
+
+var port = process.env.PORT || 8000;
+app.listen(port, (app) => {
+    console.log('listening on *: ' + port);
+})
+
